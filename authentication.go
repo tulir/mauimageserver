50f514ebb512c5c742600f0219ad2739f96ddf2a
Tulir293 <tulir293@maunium.net>
2016-01-12 12:44:08 +0200

    Implement registering

--- authentication.go
+++ authentication.go
@@ -62,5 +62,44 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement register
+	// Create a json decoder for the payload.
+	decoder := json.NewDecoder(r.Body)
+	var af AuthForm
+	// Decode the payload.
+	err := decoder.Decode(&af)
+	// Check if there was an error decoding.
+	if err != nil || len(af.Password) == 0 || len(af.Username) == 0 {
+		log.Debugf("%[1]s sent an invalid request.", getIP(r))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// Try to register
+	authToken, err := data.Register(af.Username, []byte(af.Password))
+	// Check if there was an error logging in.
+	if err != nil {
+		// Error detected.
+		if err.Error() == "userexists" {
+			log.Debugf("%[1]s tried to register the name %[2]s, but it is already in use.", getIP(r), af.Username)
+			// Username in use. Write not acceptable status.
+			w.WriteHeader(http.StatusNotAcceptable)
+		} else {
+			log.Errorf("Register error: %s", err)
+			// Other error. Write internal server error status.
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+	// Marshal the response
+	json, err := json.Marshal(AuthResponse{AuthToken: authToken})
+	// Check if there was an error marshaling the response.
+	if err != nil {
+		// Error detected. Log it.
+		log.Errorf("Failed to marshal output json to %[1]s (%[2]s): %[3]s", getIP(r), af.Username, err)
+		// Write internal serevr error status.
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Debugf("%[1]s registered and logged in as %[2]s successfully.", getIP(r), af.Username)
+	w.WriteHeader(http.StatusOK)
+	w.Write(json)
 }
