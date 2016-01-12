62772cfffd6dd3cddab2e5213f40148a078326d3
Tulir293 <tulir293@maunium.net>
2016-01-12 11:37:05 +0200

    Add AuthResponse struct

--- authentication.go
+++ authentication.go
@@ -10,6 +10,14 @@ type AuthForm struct {
 	Password string `json:"password"`
 }
 
+// AuthResponse is the generic authentication response which can be used for both, logging in and registering.
+// If the login/register was successful, the error field should not exist. In other cases, the auth token field
+// should not exist.
+type AuthResponse struct {
+	AuthToken string `json:"auth-token"`
+	Error     string `json:"error"`
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
 	// TODO: Implement login
 }
