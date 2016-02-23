6b617ad243e5414fc6faf1e868f507906a6409cd
Tulir293 <tulir293@maunium.net>
2016-02-23 15:07:46 +0200

    Bump version to Beta 3

--- mauimageserver.go
+++ mauimageserver.go
@@ -13,7 +13,7 @@ import (
 	"syscall"
 )
 
-const version = "2.0.0-B2"
+const version = "2.0.0-B3"
 
 var debug = flag.BoolP("debug", "d", false, "Enable to print debug messages to stdout")
 var confPath = flag.StringP("config", "c", "./config.json", "The path of the mauImageServer configuration file.")
